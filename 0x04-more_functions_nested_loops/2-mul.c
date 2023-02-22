80cabf9e5aa4ae15241c99ae4fd4d4349c051b3c
Confidence Ochie <ConfidenceOchie005@gmail.com>
2023-02-22 22:25:43 +0100

    All assigment are done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int to be multiplied by b
+ * @b: int to be multiplied by a
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README pointers array and strings 7
